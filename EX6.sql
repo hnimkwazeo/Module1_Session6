1db40f9fce50cb521523c8e63bd3aff03ee4beb2
hnimkwazeo <vunhatminh.vnw.03@gmail.com>
2026-03-23 17:17:36 +0700

    edit EX6

--- EX6.sql
+++ EX6.sql
@@ -1,42 +1,39 @@
-create database HotelDB;
-
-create schema hotel;
-
-create table RoomTypes(
-	room_type_id serial primary key,
-	type_name varchar(50) not null unique,
-	price_per_night numeric(10,2) check (price_per_night > 0),
-	max_capacity int check (max_capacity > 0)
+create table orders(
+	id serial primary key,
+	customer_id int,
+	order_date date,
+	total_amount numeric(10,2)
 );
 
-create table Rooms(
-	room_id serial primary key,
-	room_number varchar(10) not null unique,
-	room_type_id int references roomtypes(room_type_id),
-	status varchar(20) check (status IN ('Available','Occupied','Maintenance'))
-);
+INSERT INTO orders (customer_id, order_date, total_amount) VALUES
+(1, '2022-12-20', 200.00),
+(2, '2023-01-10', 1200.50),
+(1, '2023-03-15', 3200.00),
+(3, '2023-07-01', 5000.00),
+(4, '2024-01-10', 750.75),
+(5, '2024-02-15', 9800.00),
+(2, '2024-03-01', 450.00),
+(6, '2024-03-05', 50.00),
+(7, '2025-02-02', 25.00);
 
-create table Customers(
-	customer_id serial primary key,
-	full_name varchar(100) not null,
-	email varchar(100) unique not null,
-	phone varchar(15) not null
-);
+--Y1:
+select sum(total_amount) as "total_revenue", count(id) as "total_orders", avg(total_amount) as "average_order_value"
+from orders;
 
-create table Bookings(
-	booking_id serial primary key,
-	customer_id int references customers(customer_id),
-	room_id int references rooms(room_id),
-	check_in Date not null,
-	check_out Date not null,
-	status varchar(20) check (status IN ('Pending','Comfirmed','Cancelled'))
-);
+--Y2:
+select extract(year from order_date) as "Year", sum(total_amount) as "total_revenue"
+from orders
+group by extract(year from order_date)
+order by extract(year from order_date) asc;
 
-create table Payments(
-	payment_id serial primary key,
-	booking_id int references bookings(booking_id),
-	amount numeric(10,2) check (amount >= 0),
-	payment_date Date not null,
-	methods varchar(20) check (methods IN ('Credit Card','Cash','Bank Transfer'))
-);
+--Y3:
+select extract(year from order_date) as "Year", sum(total_amount) as "total_revenue"
+from orders
+group by extract(year from order_date)
+having  sum(total_amount) > 50
+order by extract(year from order_date) asc;
 
+--Y4:
+select * 
+from orders
+order by total_amount desc limit 5;
